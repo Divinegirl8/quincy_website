a12fb9a5e890b6cdf9ac787be99585183123892f
James DivineMercy <iamoluchimercy@gmail.com>
2024-05-02 09:19:21 +0100

    new commit

--- src/features/Books/index.jsx
+++ src/features/Books/index.jsx
@@ -12,13 +12,12 @@ const Books = () =>{
                 <div className={styles.book1}>
                     <img src={book1} alt={"book 1"}/>
                     <h3>OMEIHEUKWU: THE ONE WHO DOES EXTRAORDINARY THINGS </h3>
-                    <h4>Effortlessly and spiritually nutritious. A grandfather-grandson <span>relationship in shades you've never seen before. A solid reminder of the power that memories carry.<Link to={""} className={styles.lnk}>learn more</Link></span></h4>
+                    <h4 className={styles.wrt}>Effortlessly and spiritually nutritious. A grandfather-grandson relationship in shades you've never seen before. A solid reminder of the power that memories carry.<Link to={""} className={styles.lnk}>learn more</Link></h4>
                 </div>
 
                 <div className={styles.book2}>
                     <img src={book2} alt={"book 2"}/>
-                    <h3>THE LITERARY SEDUCER: LURING YOUR READERS INTO YOUR BOOK <span className={styles.span2}>AND KEEPING THEM SPELLBOUND FOR GOOD</span>
-                    </h3>
+                    <h3 className={styles.span2}>THE LITERARY SEDUCER: LURING YOUR READERS INTO YOUR BOOK AND KEEPING THEM SPELLBOUND FOR GOOD</h3>
                     <h4>Never give your readers a reason to toss your book aside!<Link to={""} className={styles.lnk}>learn more</Link></h4>
                 </div>
             </div>
