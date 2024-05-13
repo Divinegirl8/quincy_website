6cf2cc44e0179f96534ec6283e76bad02edb5efd
James DivineMercy <iamoluchimercy@gmail.com>
2024-05-13 08:33:55 +0100

    new commit

--- src/features/Blog/index.jsx
+++ src/features/Blog/index.jsx
@@ -1,9 +1,66 @@
 import style from "./index.module.css"
 import NavBar from "../../components/reusables/NavBar";
+import blog from "../../assests/blogImg/blog.jpeg";
 const Blog = () =>{
     return(
         <>
             <NavBar textColor={"yellow"} textColor2={"white"} textColor3={"white"} textColor4={"white"}/>
+            <div>
+                <div>
+                    <img src={blog} alt={"blog picture"}/>
+                </div>
+                <div>
+                    <p>The night Obi moved into his new flat, something came for him. He was lounging on the sofa when the knock jarred him.</p>
+                    <p>"Who is there?" he asked, springing to his feet.</p>
+                    <p>A bright flash of lightning lit the sitting room, and thunder rumbled as if the skies were setting a wild beast loose.</p>
+                    <p>"Who is there?" he asked again, but no one answered.</p>
+                    <p>Then he ambled to the door and listened. Footsteps were echoing, chains rattling, and the air was pregnant with the alien smell of smoke.</p>
+                    <p>"Who is there?" Obi asked yet again.</p>
+                    <p>"Your neighbor," a silvery voice finally answered.</p>
+                    <p>Obi glanced sideways before pulling the door handle, but there was no one there. He peeked into the hallway and shook his head, his breath escaping in a quick sigh.</p>
+                    <p>He was about to shut the door when a sudden burst of smoke gripped him and hurled him to the floor, almost sucking air out from his body.</p>
+                    <p>"Obimma, right?" A silhouetted figure with chains stood in the doorway.</p>
+                    <p>Obi winced as he struggled to rise.</p>
+                    <p>"How are you still conscious?" The silhouetted figure growled. "No one in this building has ever survived my blow."</p>
+                    <p>Obi was kneeling, still trying to gain balance.</p>
+                    <p>The figure lunged forward this second and staggered backward the next second as though there was an invisible shield in the doorway.</p>
+                    <p>"Why can't I get in?" it yelled.</p>
+                    <p>Obi was still on his knees but revivified.</p>
+                    <p>With head raised, he said, "Because."</p>
+                    <p>With one knee bent, he said, "I am."</p>
+                    <p>And with legs up, he said, "New and different."</p>
+                    <p>Then he grabbed the door and slammed it against the figure.</p>
+                    <p>Thunder started to rage as if provoked again.</p>
+                    <p>"I'll be back!" the figure roared. </p>
+                    <p>"And I'll be ready!" Obi fired back.</p>
+                    <p>*</p>
+                    <p>Fiction?</p>
+                    <p>Yes.</p>
+                    <p>But let's quickly use this short piece to dish out some vital lessons.</p>
+                    <p>*</p>
+                    <p>Now, while some of us share a certain resemblance with Obi, some don't.</p>
+                    <p>How so?</p>
+                    <p>Let's take Obi's new apartment as a new level (a new phase) and the silhouetted figure as a new challenge.</p>
+                    <p>Now, why do some of us pull up once we crush a certain goal―once we attain a certain level in life?</p>
+                    <p>Why do we buy into the notion that once we've accomplished a given task, then that's it, no more tasks ahead?</p>
+                    <p>Truth be told, Life is a journey filled with quests. Ergo, if you're going to pull up at some point to catch your breath, pull up.</p>
+                    <p>But do keep in mind that it is not your final destination. Bear in mind that getting to a certain point doesn't equate to being free of challenges.</p>
+                    <p>Whether or not you keep moving right away, you'll no doubt come face to face with challenges. Some will be small and others will loom large.</p>
+                    <p>And you have to be ready. The best way to get through them is to be prepared for any situation that may be hurled your way.</p>
+                    <p>I know if some of us had a choice, we would probably seek to avoid challenges.</p>
+                    <p>However, despite one's best effort to avoid them, challenges arise and threaten one's journey. Thus, one must handle them.</p>
+                    <p>It is best to be prepared for road bumps because you're bound to hit them from time to time as you go through life.</p>
+                    <p>Don't arrive at a level in your life and get too comfortable with it. Besides, there is comfort in discomfort.</p>
+                    <p>Yes.</p>
+                    <p>And growth starts with a decision to move beyond your present circumstances.</p>
+                    <p>"When things do not go your way, remember that every challenge — every adversity — contains within it the seeds of opportunity and growth." ― Roy T. Bennett.</p>
+                    <p>A challenge only becomes an [absolute] obstacle when you bow to it.</p>
+                    <p>Start that fire and keep it burning.</p>
+                    <p>Have a high-yielding week!</p>
+                    <h4>© Quincy Alagbu</h4>
+
+                </div>
+            </div>
         </>
     )
 }
