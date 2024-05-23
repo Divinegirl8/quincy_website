1f7a9da90b777d75ee14b70eb9fb69df6d735ab8
James DivineMercy <iamoluchimercy@gmail.com>
2024-05-23 15:03:24 +0100

    new commit

--- src/components/reusables/Footer/index.jsx
+++ src/components/reusables/Footer/index.jsx
@@ -1,6 +1,13 @@
+import style from "./"
 const Footer = () => {
     return(
-        <></>
+        <>
+        <div className={style.mainContainer}>
+            <div>
+
+            </div>
+        </div>
+        </>
     )
 }
 
