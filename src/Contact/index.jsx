d330238310d90f64279c0a7c487bf0af19902216
James DivineMercy <iamoluchimercy@gmail.com>
2024-07-03 10:18:51 +0100

    new commit

---
+++ src/Contact/index.jsx
@@ -0,0 +1,12 @@
+import style from "./index.module.css"
+import {Link} from "react-router-dom";
+
+
+const Contact = () =>{
+    return(
+        <>
+        <Link to={""}></Link>
+        </>
+    )
+}
+export default Contact
